0491bcc170e27e82bda171f57ae238b11649c645
Abel <abelvanbeek@hotmail.com>
2020-10-28 16:27:27 +0100

    Added mood alteration

--- memory/memory.py
+++ memory/memory.py
@@ -5,14 +5,15 @@ path.append('..')
 from .moods.moods import raw_moods, moods
 from config.config import config
 import random
+import time
 
 class Memory:
     def __init__(self):
         self.current_mood = moods[config.props['drone']['init_mood']]
-        self.lost_cnt = 0
+        self.lost_state = 0, False
     
     def get_mood(self):
-        return self.current_mood
+        return self.current_mood.name
     
     def set_mood(self, mood_name):
         self.current_mood = moods[mood_name]
@@ -21,15 +22,41 @@ class Memory:
         # Select random mood excluding the last mood, which is the 'confused' mood, which is only used when the drone is lost.
         id = random.randint(0, len(moods) - 1)
         self.set_mood(raw_moods[id].name)
+        # self.set_mood('sad')
 
     def found(self):
-        self.lost_cnt = 0
-        self.select_random_mood
+        if self.lost_state[1]:
+            # Found itself again
+            self.lost_state = 0, False
+            self.select_random_mood()
+            self.apply_new_mood()
+    
+    def apply_new_mood(self):
+        mood = self.get_mood()
+
+        # Ensure that the drone is flying
+        if not config.drone_flying:
+            config.drone_takeoff()
+
+        if mood == 'happy':
+            config.drone.set_speed(100)
+            config.drone.flip_back()
+        elif mood == 'sad':
+            config.drone.set_speed(10)
+            if random.random() < 0.5:
+                config.drone.flip_left()
+                config.drone_emergency_stop()
+                time.sleep(7)
+                config.drone_takeoff()
+        elif mood == 'no_mood':
+            config.drone.set_speed(50)
 
     def lost(self):
-        self.lost_cnt += 1
+        self.lost_state = self.lost_state[0] + 1, self.lost_state[1]
 
-        if self.lost_cnt > config.props['drone']['lost_frame_cnt']:
+        # If the drone was not already lost and the # of frames in which no object has been found has exceeded the threshold 
+        if not self.lost_state[1] and self.lost_state[0] > config.props['drone']['lost_frame_cnt']:
+            self.lost_state = self.lost_state[0], True
             self.current_mood = moods['confused']
     
     def behave(self):
